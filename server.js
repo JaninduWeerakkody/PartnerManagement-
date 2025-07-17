6456cf3b6b36dc2c521002728266668c7bdc28ee
JaninduWeerakkody <janindu.20230797@iit.ac.lk>
2025-07-17 18:31:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/tmf668';
+const MONGODB_URI = process.env.MONGO_URL || 'mongodb://localhost:27017/tmf668';
+
+console.log('MONGO_URL:', process.env.MONGO_URL);
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
